e0b71ae06a43e71e42e3537a149ed9961fc290dc
imAdithya <168222090+imAdithya@users.noreply.github.com>
2026-05-18 10:55:48 +0530

    Delete verif directory

--- verif/bridge/tb/ethmac/tb/dma_sequencer.sv
+++
@@ -1,7 +0,0 @@
-class dma_sequencer extends uvm_sequencer #(wishbone_transaction);
-  `uvm_component_utils(dma_sequencer)
-
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction
-endclass
